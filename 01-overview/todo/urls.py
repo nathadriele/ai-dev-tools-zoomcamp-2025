8f77fecb56b53007199b6536e8a78990b95895ca
Nathalia Adriele <nat-adriele@hotmail.com>
2025-12-01 15:59:18 +0000

    app

--- 01-overview/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TodoListView.as_view(), name='todo-list'),
-    path('create/', views.TodoCreateView.as_view(), name='todo-create'),
-    path('<int:pk>/update/', views.TodoUpdateView.as_view(), name='todo-update'),
-    path('<int:pk>/delete/', views.TodoDeleteView.as_view(), name='todo-delete'),
-]
